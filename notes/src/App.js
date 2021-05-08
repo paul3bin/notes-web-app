c0ea1ec11e0289ae4dde90a63774b0bd719138c8
Ebin Paul <ebinpaul@outlook.com>
2021-05-08 18:15:59 +0530

    Update App.js

--- notes/src/App.js
+++ notes/src/App.js
@@ -6,11 +6,15 @@ import { Home } from "./contents/home";
 import { Login } from "./contents/login";
 import { SignUp } from "./contents/signup";
 import { NewNote } from "./contents/newNote";
+import { Landing } from "./contents/landing";
 
 function App() {
   return (
     <Router>
       <Route exact path="/">
+        <Landing />
+      </Route>
+      <Route exact path="/home">
         <Home />
       </Route>
       <Route exact path="/new">
