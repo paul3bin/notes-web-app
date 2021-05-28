cb911a35088d84f4acdf5555292cddcfeb841f38
Ebin Paul <ebinpaul@outlook.com>
2021-05-28 18:50:54 +0530

    Update signup.js

--- notes/src/contents/signup.js
+++ notes/src/contents/signup.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Nav from "../components/navbar";
 
 export function SignUp() {
   return (
-    <div>
-      <h1>SignUp</h1>
+    <div className="d-flex h-100 text-center text-white bg-dark">
+      <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <Nav />
+      </div>
     </div>
   );
 }
