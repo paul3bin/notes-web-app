2ca9ba415eab0ffed3445dcf6c84180bfd980928
Ebin Paul <ebinpaul@outlook.com>
2021-05-29 09:49:38 +0530

    Update navbar.js

--- notes/src/components/navbar.js
+++ notes/src/components/navbar.js
@@ -3,17 +3,18 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <header class="mb-auto">
+    <header className="mb-auto">
       <div>
-        <h3 class="float-md-start mb-0">Notes</h3>
-        <nav class="nav nav-masthead justify-content-center float-md-end">
-          <Link class="nav-link active" aria-current="page" to="/">
-            Home
+        <h3 className="float-md-start mb-0">
+          <Link className="text-light text-deco fs-2" to="/">
+            Notes
           </Link>
-          <Link class="nav-link active" aria-current="page" to="/login">
+        </h3>
+        <nav className="nav nav-masthead justify-content-center float-md-end">
+          <Link className="nav-link active" aria-current="page" to="/login">
             Login
           </Link>
-          <Link class="nav-link active" aria-current="page" to="/signup">
+          <Link className="nav-link active" aria-current="page" to="/signup">
             Sign Up
           </Link>
         </nav>
