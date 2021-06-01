7f18fce0308affa6bbcd13c7e49fa651b4f0d0e5
Ebin Paul <ebinpaul@outlook.com>
2021-06-01 08:38:22 +0530

    Update login.js

--- notes/src/contents/login.js
+++ notes/src/contents/login.js
@@ -1,57 +1,87 @@
 import React from "react";
-import Nav from "../components/navbar";
-import Footer from "../components/landing_footer";
+import { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
-import { Link } from "react-router-dom";
+import Wrapper from "../components/wrapper";
+import { API } from "../Api";
+
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import "../styles/landing.css";
 
 export function Login() {
   document.title = "Notes | Login";
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [token, setToken] = useCookies(["token"]);
+  const [userID, setUserID] = useCookies(["uid"]);
+
+  const history = useHistory();
+  const loginClicked = () => {
+    API.loginUser({ email: email, password: password })
+      .then((resp) => [
+        setToken("token", resp.token),
+        setUserID("uid", resp.user_id),
+      ])
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    if (token["token"] === "undefined") {
+      toast.error("Wrong username or password.", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      setToken("token", "");
+      setEmail("");
+      setPassword("");
+    } else {
+      if (token["token"]) history.push("/home");
+    }
+  }, [token]);
+
   return (
-    <div className="d-flex h-100 text-center text-white bg-dark bg-gradient">
-      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-        <Nav />
-
-        <div className="card text-light bg-dark mb-3 shadow p-3 mb-5 rounded">
-          <div className="card-header">Login</div>
-          <div className="card-body">
-            {/* <h5 className="card-title">Dark card title</h5> */}
-            <p className="card-text">
-              <form>
-                <div class="form-floating mb-3 text-dark">
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="floatingInput"
-                    placeholder="name@example.com"
-                  />
-                  <label for="floatingInput">Email address</label>
-                </div>
-                <div class="form-floating mb-3 text-dark">
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="floatingPassword"
-                    placeholder="Password"
-                  />
-                  <label for="floatingPassword">Password</label>
-                </div>
-                <p>
-                  Don't have an account? Register <Link to="/signup">here</Link>
-                  .
-                </p>
-                <button type="submit" class="btn btn-primary">
-                  Submit
-                </button>
-              </form>
-            </p>
-          </div>
+    <Wrapper>
+      <div className="card text-light bg-dark mb-3 shadow p-3 mb-5 rounded">
+        <div className="card-header">Login</div>
+        <div className="card-body">
+          <p className="card-text">
+            <div>
+              <div class="form-floating mb-3 text-dark">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="floatingInput"
+                  placeholder="name@example.com"
+                  required={true}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <label for="floatingInput">Email address</label>
+              </div>
+              <div class="form-floating mb-3 text-dark">
+                <input
+                  type="password"
+                  class="form-control"
+                  id="floatingPassword"
+                  placeholder="Password"
+                  required={true}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <label for="floatingPassword">Password</label>
+              </div>
+              <p>
+                Don't have an account? Register <Link to="/signup">here</Link>.
+              </p>
+              <button class="btn btn-primary" onClick={loginClicked}>
+                Submit
+              </button>
+            </div>
+          </p>
         </div>
-
-        <Footer />
       </div>
-    </div>
+    </Wrapper>
   );
 }
