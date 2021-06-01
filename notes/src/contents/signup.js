87aa8ef9a25a45238415e6715776da50886b1340
Ebin Paul <ebinpaul@outlook.com>
2021-06-01 08:38:28 +0530

    Update signup.js

--- notes/src/contents/signup.js
+++ notes/src/contents/signup.js
@@ -1,63 +1,136 @@
-import React from "react";
-import Nav from "../components/navbar";
-import Footer from "../components/landing_footer";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
-import { Link } from "react-router-dom";
+import Wrapper from "../components/wrapper";
+import { API } from "../Api";
+
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export function SignUp() {
   document.title = "Notes | Sign up";
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordReEnter, setPasswordReEnter] = useState("");
+  const history = useHistory();
+
+  const validEmailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  const passwordCheck = () => {
+    return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(password);
+  };
+
+  const passwordReEnterCheck = () => {
+    return password.length === passwordReEnter.length;
+  };
+
+  const registerClicked = () => {
+    if (name.length !== 0) {
+      if (passwordCheck && passwordReEnterCheck) {
+        if (email.match(validEmailRegex)) {
+          API.registerUser({ email, password, name })
+            .then((resp) =>
+              resp.email[0] === "user with this email already exists."
+                ? toast.error(
+                    "User already exists!",
+                    {
+                      position: toast.POSITION.TOP_RIGHT,
+                    },
+                    (setEmail(""),
+                    setPassword(""),
+                    setPasswordReEnter(""),
+                    setName(""))
+                  )
+                : toast.success(
+                    "New user registered. Now, login with the same credentials.",
+                    {
+                      position: toast.POSITION.TOP_RIGHT,
+                    },
+                    (setEmail(""),
+                    setPassword(""),
+                    setPasswordReEnter(""),
+                    setName(""))
+                  )
+            )
+            .catch((error) => console.log(error));
+        }
+      } else {
+        toast.error(
+          "Check your password and try again.",
+          {
+            position: toast.POSITION.TOP_RIGHT,
+          },
+          (setEmail(""), setPassword(""), setPasswordReEnter(""), setName(""))
+        );
+      }
+    }
+  };
+
   return (
-    <div className="d-flex h-100 text-center text-white bg-dark bg-gradient">
-      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-        <Nav />
-
-        <div className="card text-light bg-dark shadow p-3 mb-5 rounded">
-          <div className="card-header">Sign-Up</div>
-          <div className="card-body">
-            {/* <h5 className="card-title">Dark card title</h5> */}
-            <p className="card-text">
-              <form>
-                <div class="form-floating mb-3 text-dark">
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="floatingInput"
-                    placeholder="name@example.com"
-                  />
-                  <label for="floatingInput">Email address</label>
-                </div>
-                <div class="form-floating mb-3 text-dark">
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="floatingPassword"
-                    placeholder="Password"
-                  />
-                  <label for="floatingPassword">Password</label>
-                </div>
-                <div class="form-floating mb-3 text-dark">
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="floatingPassword"
-                    placeholder="Password"
-                  />
-                  <label for="floatingPassword">Re-Enter Password</label>
-                </div>
-                <p className="p-login-register">
-                  Already have an account? Login <Link to="/login">here</Link>.
-                </p>
-                <button type="submit" class="btn btn-primary">
-                  Submit
-                </button>
-              </form>
-            </p>
-          </div>
+    <Wrapper>
+      <ToastContainer />
+      <div className="card text-light bg-dark shadow p-3 mb-5 rounded">
+        <div className="card-header">Sign-Up</div>
+        <div className="card-body">
+          <p className="card-text">
+            <div>
+              <div className="form-floating mb-3 text-dark">
+                <input
+                  type="Text"
+                  className="form-control"
+                  id="floatingInput"
+                  placeholder="John Doe"
+                  required={true}
+                  onChange={(evt) => setName(evt.target.value)}
+                />
+                <label for="floatingInput">Full Name</label>
+              </div>
+              <div class="form-floating mb-3 text-dark">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="floatingInput"
+                  placeholder="name@example.com"
+                  required={true}
+                  onChange={(evt) => setEmail(evt.target.value)}
+                />
+                <label for="floatingInput">Email address</label>
+              </div>
+              <div class="form-floating mb-3 text-dark">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="floatingPassword"
+                  placeholder="Password"
+                  required={true}
+                  onChange={(evt) => setPassword(evt.target.value)}
+                />
+                <label for="floatingPassword">Password</label>
+              </div>
+              <div class="form-floating mb-3 text-dark">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="floatingPassword"
+                  placeholder="Password"
+                  required={true}
+                  onChange={(evt) => setPasswordReEnter(evt.target.value)}
+                />
+                <label for="floatingPassword">Re-Enter Password</label>
+              </div>
+              <p className="p-login-register">
+                Already have an account? Login <Link to="/login">here</Link>.
+              </p>
+              <button class="btn btn-primary" onClick={registerClicked}>
+                Submit
+              </button>
+            </div>
+          </p>
         </div>
-
-        <Footer />
       </div>
-    </div>
+    </Wrapper>
   );
 }
