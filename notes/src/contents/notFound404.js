5426e0c5dc014e6bccae7c880d379933877104d6
Ebin Paul <ebinpaul@outlook.com>
2021-06-04 09:54:43 +0530

    Create notFound404.js

---
+++ notes/src/contents/notFound404.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "../styles/landing.css";
+
+// import Wrapper from "../components/wrapper";
+
+export function PageNotFound() {
+  document.title = "Notes | Welcome";
+
+  return (
+    <div className="d-flex h-100 text-center text-white bg-dark bg-gradient">
+      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <main className="px-3">
+          <h1>404 | Page not found!</h1>
+          <p className="lead">Login to continue.</p>
+          <p class="lead">
+            <Link
+              to="/login"
+              class="btn btn-lg btn-secondary fw-bold border-white bg-white"
+            >
+              Login
+            </Link>
+          </p>
+        </main>
+      </div>
+    </div>
+  );
+}
