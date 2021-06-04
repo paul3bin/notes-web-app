8920c0b46c979980a27a4503ea4e91c3b8e1e2f6
Ebin Paul <ebinpaul@outlook.com>
2021-06-04 09:54:46 +0530

    Update ProtectedRoute.js

--- notes/src/ProtectedRoute.js
+++ notes/src/ProtectedRoute.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 import { useCookies } from "react-cookie";
+import { PageNotFound } from "./contents/notFound404";
 
 function ProtectedRoute(props) {
   const Component = props.component;
   const [token] = useCookies(["token"]);
   const isAuthenticated = token["token"];
 
-  return isAuthenticated ? <Component /> : <Redirect to={{ pathname: "/" }} />;
+  return isAuthenticated ? <Component /> : <PageNotFound />;
+  // return isAuthenticated ? <Component /> : <Redirect to={{ pathname: "/" }} />;
 }
 
 export { ProtectedRoute };
