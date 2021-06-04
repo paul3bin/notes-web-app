a0f8feac811355dd2e2627737605d733fd84702b
Ebin Paul <ebinpaul@outlook.com>
2021-06-04 10:17:48 +0530

    Update PrivateNav.js

--- notes/src/components/PrivateNav.js
+++ notes/src/components/PrivateNav.js
@@ -21,18 +21,6 @@ const PrivateNav = () => {
       <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
         Notes
       </a>
-      <button
-        class="navbar-toggler position-absolute d-md-none collapsed"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#sidebarMenu"
-        aria-controls="sidebarMenu"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse"></div>
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
           <a class="nav-link" href="#" onClick={logoutAction}>
