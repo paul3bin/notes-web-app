5bf6f810c0ac65d5e36482488a9c4325b50ebb2c
Ebin Paul <ebinpaul@outlook.com>
2021-06-04 09:54:36 +0530

    Create PrivateNav.js

---
+++ notes/src/components/PrivateNav.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useCookies } from "react-cookie";
+
+import "../styles/home.css";
+
+const PrivateNav = () => {
+  const [token, setToken, deleteToken] = useCookies(["token"]);
+  const [userID, setUserID, deleteUserID] = useCookies(["uid"]);
+
+  const history = useHistory();
+
+  const logoutAction = () => {
+    deleteToken(["token"]);
+    deleteUserID(["uid"]);
+    history.push("/");
+  };
+
+  return (
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
+        Notes
+      </a>
+      <button
+        class="navbar-toggler position-absolute d-md-none collapsed"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#sidebarMenu"
+        aria-controls="sidebarMenu"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse"></div>
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="#" onClick={logoutAction}>
+            Sign out
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default PrivateNav;
