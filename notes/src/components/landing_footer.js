c4916ccdbc2ba42c46e0d8b464f41adcda089265
Ebin Paul <ebinpaul@outlook.com>
2021-06-05 18:31:28 +0530

    Create landing_footer.js

---
+++ notes/src/components/landing_footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="mt-auto text-white-50">
+      <p>
+        Made with love by{" "}
+        <a href="Your social handle" className="text-white">
+          @paul_ebin
+        </a>
+        .
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
