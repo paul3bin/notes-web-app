feff23efdd2be599c5cfdc0a1cc6f124f4a2bcef
Ebin Paul <ebinpaul@outlook.com>
2021-06-05 18:32:10 +0530

    Update Api.js

--- notes/src/Api.js
+++ notes/src/Api.js
@@ -18,4 +18,56 @@ export class API {
       body: JSON.stringify(body),
     }).then((resp) => resp.json());
   }
+
+  static getNotes(token) {
+    return fetch("http://localhost:8000/api/note/get", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${token}`,
+      },
+    }).then((resp) => resp.json());
+  }
+
+  static addNewNote(body, token) {
+    return fetch("http://localhost:8000/api/note/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${token}`,
+      },
+      body: JSON.stringify(body),
+    }).then((resp) => resp.json());
+  }
+
+  static getNoteDetails(token, note_id) {
+    return fetch(`http://localhost:8000/api/note/get/${note_id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${token}`,
+      },
+    }).then((resp) => resp.json());
+  }
+
+  static updateNote(note_id, body, token) {
+    return fetch(`http://localhost:8000/api/note/update/${note_id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${token}`,
+      },
+      body: JSON.stringify(body),
+    }).then((resp) => resp.json());
+  }
+
+  static deleteTask(note_id, token) {
+    return fetch(`http://localhost:8000/api/note/delete/${note_id}/`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${token}`,
+      },
+    });
+  }
 }
