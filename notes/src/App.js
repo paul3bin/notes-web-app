bc1416e92c5cba45864ef07485fbea3e45a9630e
Ebin Paul <ebinpaul@outlook.com>
2022-07-04 16:08:58 +0530

    changes to include route for not found page

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,26 +1,32 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { Home } from "./contents/home";
 import { Login } from "./contents/login";
 import { SignUp } from "./contents/signup";
 import { Landing } from "./contents/landing";
 import { ProtectedRoute } from "./ProtectedRoute";
+import { PageNotFound } from "./contents/notFound404";
 
 function App() {
   return (
     <Router>
-      <Route exact path="/">
-        <Landing />
-      </Route>
-      <Route exact path="/login">
-        <Login />
-      </Route>
-      <Route exact path="/signup">
-        <SignUp />
-      </Route>
-      <ProtectedRoute exact path="/home" component={Home} />
+      <Switch>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/signup">
+          <SignUp />
+        </Route>
+        <ProtectedRoute exact path="/home" component={Home} />
+        <Route>
+          <PageNotFound />
+        </Route>
+      </Switch>
     </Router>
   );
 }
